2188f8916fff46eef703471b6e8a8242005d9187
SIST <SIST@DESKTOP-J1EES6D>
2023-09-07 16:16:28 +0900

    .

---
+++ FirstSpringProject/src/main/java/com/sist/dao/EmpDAO.java
@@ -0,0 +1,14 @@
+package com.sist.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+@Repository("dao")
+public class EmpDAO {
+	@Autowired
+	private EmpMapper mapper;
+	public List<EmpVO> empListData(){
+		return mapper.empListData();
+	}
+}
---
+++ FirstSpringProject/src/main/java/com/sist/web/MainClass.java
@@ -0,0 +1,20 @@
+package com.sist.web;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sist.dao.EmpDAO;
+import com.sist.dao.EmpVO;
+
+public class MainClass {
+	public static void main(String[] args) {
+		ApplicationContext app=new ClassPathXmlApplicationContext("application.xml");
+		EmpDAO dao=(EmpDAO)app.getBean("dao");
+		List<EmpVO> list=dao.empListData();
+		for(EmpVO vo:list) {
+			System.out.println(vo.getEname());
+		}
+	}
+}
